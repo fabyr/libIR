9c269a3a587f631ddc959ead7e86fa151a9108a7
fabyr <fabyr@proton.me>
2023-05-28 05:55:27 +0200

    NN & Refactor

--- make.sh
+++ make.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-make clean
-LDFLAGS="-fPIC -lm -shared" make
+
+
+
+LIBTORCH="/home/fabyr/Desktop/libIR/libtorch"
+
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$LIBTORCH ..
+cmake --build . --config Release
