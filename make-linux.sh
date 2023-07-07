8a2fde96632402059e8acb935a50b720f745ced8
fabyr <fabyr@proton.me>
2023-07-07 15:05:33 +0200

    Build scripts

---
+++ make-linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf build
+mkdir build
+cd build
+
+export SINGLE_PRECISION=1
+
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake --build . --config Release
+
+cp libIR.so ../libirsharp/libs/
