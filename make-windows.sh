9e27851037f0d5903766752647d2227cf5b7f76a
fabyr <fabyr@proton.me>
2023-07-08 20:04:41 +0200

    Add error output

--- make-windows.sh
+++ make-windows.sh
@@ -9,4 +9,10 @@ export SINGLE_PRECISION=1
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cmake --build . --config Release
 
+ERRORCODE=$?
+if [ $ERRORCODE -ne 0 ]; then
+    echo "Failed."
+    exit 1
+fi
+
 cp libIR.dll ../libirsharp/libs/IR.dll
